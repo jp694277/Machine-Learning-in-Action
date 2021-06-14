6e016d1cddaf904f662d0c420735ad349cb9f2c3
Debbie Wang (Ziwen) <jp694277@gmail.com>
2021-06-14 15:59:25 +0800

    Add files via upload

---
+++ kNN.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from numpy import *
+import operator
+from os import listdir
+
+
+def createDataSet():
+            group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+            labels = ['A','A','B','B']
+            return group, labels
+ 
+        
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0] #計算歐式距離
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()
+    classCount={}
+    for i in range(k): #選擇距離最小的k個值
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True) #排序
+    return sortedClassCount[0][0]
+
+
+def file2matrix(filename):
+    love_dictionary={'largeDoses':3, 'smallDoses':2, 'didntLike':1}
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)            #get the number of lines in the file
+    returnMat = zeros((numberOfLines,3))        #prepare matrix to return
+    classLabelVector = []                       #prepare labels return   
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        if(listFromLine[-1].isdigit()):
+            classLabelVector.append(int(listFromLine[-1]))
+        else:
+            classLabelVector.append(love_dictionary.get(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelVector
+
+    
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m,1))
+    normDataSet = normDataSet/tile(ranges, (m,1))   #element wise divide
+    return normDataSet, ranges, minVals
+   
+
+
+def img2vector(filename):
+    returnVect = zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+    return returnVect
